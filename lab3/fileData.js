e9cdb2248ba9d88a5623e4b61ba16fbb797d0053
mfreema1 <mfreema1@stevens.edu>
2018-09-14 18:16:07 -0400

    Setup project structure

---
+++ lab3/fileData.js
@@ -0,0 +1,15 @@
+const getFileAsString = async (path) => {
+
+};
+
+const getFileAsJSON = async (path) => {
+
+};
+
+const saveStringToFile = async (path, text) => {
+
+};
+
+const saveJSONToFile = async (path, obj) => {
+
+};
---
+++ lab3/textMetrics.js
@@ -0,0 +1,3 @@
+const createMetrics = text => {
+    
+};
