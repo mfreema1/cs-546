a518c8be9c3e9cfdcc4afb1176fb2e536ba24780
mfreema1 <mfreema1@stevens.edu>
2018-09-19 12:43:04 -0400

    Update file processing

--- lab3/fileData.js
+++ lab3/fileData.js
@@ -1,15 +1,43 @@
-const getFileAsString = async (path) => {
+const bluebird = require('bluebird');
+const Promise = bluebird.Promise;
+const fs = Promise.promisifyAll(require('fs'));
 
+const _checkPathValidity = path => {
+    if(!path)
+        Throw('Need a valid file path');
+};
+
+const getFileAsString = async (path) => {
+    _checkPathValidity(path);
+    await fs.readFileAsync(path, (err, data) => {
+        if(err)
+            Throw(err);
+        else
+            return data;
+    });
 };
 
 const getFileAsJSON = async (path) => {
-
+    _checkPathValidity(path);
+    try{
+        return JSON.parse(await getFileAsString(path));
+    }
+    catch(e) {
+        Throw('File is not valid JSON');
+    }
 };
 
 const saveStringToFile = async (path, text) => {
-
+    _checkPathValidity(path);
+    await fs.writeFileAsync(path, text, (err, data) => {
+        if(err)
+            Throw(err);
+        else
+            return data;
+    });
 };
 
 const saveJSONToFile = async (path, obj) => {
-
+    _checkPathValidity(path);
+    return await saveStringToFile(JSON.stringify(obj));
 };
