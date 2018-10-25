2a138299a8f9a4307dd160e009a8f256f4784867
mfreema1 <mfreema1@stevens.edu>
2018-10-25 16:33:59 -0400

    Finish lab8

--- lab8/palindrome.js
+++ lab8/palindrome.js
@@ -1,9 +1,12 @@
 const checkPalindrome = async (params) => {
     let str = params['text-to-test'];
 
-    if(!str) throw "You must provide some valid text for your string";
+    if(!str) throw "Text not found in request body";
 
     str = _cleanInput(str);
+
+    if(!str) throw "Invalid text supplied in request body";
+    
     return str === str.split('').reverse().join('');
 };
 
