588d5ad6b5efb8ed785ab8d955a0e3b8b3a5e918
mfreema1 <mfreema1@stevens.edu>
2018-11-09 09:06:19 -0500

    Continue work on lab10

--- lab10/app.js
+++ lab10/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const exphbs = require("express-handlebars");
 const uuidv4 = require("uuid/v4");
 const auth = require("./auth");
 const app = express();
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 const expireCookie = res => {
   const oneHourAgo = new Date();
@@ -29,45 +31,37 @@ const pullBackCookie = (req, res, next) => {
     const sessionValid = auth.checkSession(cookie);
     if (sessionValid) next();
     else {
-      //expire the cookie
       expireCookie(res);
-      res.status(403).sendFile(path.join(__dirname, "forbidden.html"));
+      res.status(403).render("layouts/link", { title: "Forbidden" });
     }
-  } else res.status(403).sendFile(path.join(__dirname, "forbidden.html"));
+  } else res.status(403).render("layouts/link", { title: "Forbidden" });
 };
 
-//we make a middleware for checking auth
 app.get("/", pushThroughCookie, (req, res) => {
-  res.sendFile(path.join(__dirname, "login.html"));
+  res.render("layouts/home");
 });
 
 app.post("/login", pushThroughCookie, (req, res) => {
-  //hash and auth user
   const user = auth.getUserIndex(req.body.username);
   //bcrypt isn't working for some reason...
   // if(user && auth.passwordsMatch(req.body.password, auth.getUserAtIndex(user).hashedPassword)) {
   if (user && req.body.password === auth.getUserAtIndex(user).hashedPassword) {
     //authed, generate a session
     const sessionId = uuidv4();
-    //store session on user and cookie
     auth.setSession(user, sessionId);
     res.cookie("authCookie", sessionId);
     res.redirect("/private");
-  } else {
-    res.redirect("/?failure=true");
-  }
+  } else res.render("layouts/home", { error: true });
 });
 
 app.get("/private", pullBackCookie, (req, res) => {
   const cookie = req.cookies["authCookie"];
-  user = auth.getUserFromSession(cookie);
-  delete user.hashedPassword;
-  res.send(user);
+  res.render("layouts/user", auth.getUserFromSession(cookie));
 });
 
 app.get("/logout", pullBackCookie, (req, res) => {
   expireCookie(res);
-  res.sendFile(path.join(__dirname, "logout.html"));
+  res.render("layouts/link", { title: "Logged Out" });
 });
 
 const port = 3000;
